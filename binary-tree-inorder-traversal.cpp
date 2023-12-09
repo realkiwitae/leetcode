2ffa5ed36aff731df002d650bcbb02b5c0056672
realkiwitae <taelefebvre@gmail.com>
2023-12-09 06:28:31 +0100

    leetcode daily 20231209

---
+++ binary-tree-inorder-traversal.cpp
@@ -0,0 +1,35 @@
+#include <vector>
+
+using namespace std;
+
+/*
+    Time complexity O(n) (n is size of root)
+    Space complexity O(n) callstack
+*/
+
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+public:
+
+    void dfs(TreeNode* root,vector<int>&ans){
+        if(!root)return;
+        dfs(root->left,ans);
+        ans.push_back(root->val);
+        dfs(root->right,ans);
+    }
+
+    vector<int> inorderTraversal(TreeNode* root) {
+        vector<int> ans;
+        dfs(root,ans);
+        return ans;
+    }
+};
