e14e588d859d04493397db5f4e1c408ecdb00b04
realkiwitae <taelefebvre@gmail.com>
2023-12-10 18:40:19 +0100

    leetcode daily 20231210

---
+++ transpose-matrix.cpp
@@ -0,0 +1,26 @@
+#include <vector>
+
+using namespace std;
+
+/*
+    Time complexity O(m*n) 
+    Space complexity O(m*n)
+
+*/
+
+class Solution {
+public:
+    vector<vector<int>> transpose(vector<vector<int>>& matrix) {
+        int m = matrix.size();
+        int n = matrix[0].size();
+
+        vector<vector<int>> ans(n,vector<int>(m));
+
+        for(int i = 0; i < m;i++){
+            for(int j = 0; j <n;j++){
+                ans[j][i] = matrix[i][j];
+            }
+        }
+        return ans;
+    }
+};
