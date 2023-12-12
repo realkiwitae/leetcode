ba9279856091cc5657df71556b8a529dd74500cb
realkiwitae <taelefebvre@gmail.com>
2023-12-12 08:41:26 +0100

    leetcode weekly 202312 d12

---
+++ maximum-product-of-two-elements-in-an-array.cpp
@@ -0,0 +1,13 @@
+#include <vector>
+#include <algorithm>
+#include <functional>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxProduct(vector<int>& nums) {
+        sort(nums.begin(),nums.end(),greater<int>());
+        return (nums[0]-1)*(nums[1]-1);
+    }
+};
