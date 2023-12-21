271d5560a0f62858b84b0ed2d93acc542eb9f960
realkiwitae <taelefebvre@gmail.com>
2023-12-21 12:06:34 +0100

    leetcode weekly 202312 w3

---
+++ hard/best-meeting-point.cpp
@@ -0,0 +1,42 @@
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+/*
+    Time complexity O(m*n + klogk) (m is number of rows, n is number of cols, k is number of 1s)
+    Space complexity O(k)
+*/
+
+class Solution {
+public:
+    int minDist(vector<int> rows){
+        int ans = 0;
+        int L = 0;
+        int R = rows.size()-1;
+
+        while(L < R){
+            ans += rows[R--] - rows[L++];
+        }
+        return ans;
+    }
+    int minTotalDistance(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+
+        vector<int> rows;
+        vector<int> cols;
+
+        for(int i = 0; i < m; i++) {
+            for(int j = 0; j < n; j++){
+                if(!grid[i][j])continue;
+                rows.push_back(i);
+                cols.push_back(j);
+            }
+        }
+
+        sort(cols.begin(),cols.end());
+        
+        return minDist(rows)+minDist(cols);
+    }
+};
