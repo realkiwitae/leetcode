a0f1988f69b681c13c73c93b904f626ac0b280d1
realkiwitae <taelefebvre@gmail.com>
2024-01-10 16:38:48 +0100

    leetcode 0110

---
+++ medium/amount-of-time-for-binary-tree-to-be-infected.cpp
@@ -0,0 +1,38 @@
+#include "utils.h"
+
+/*
+    * leeetcode: 1530 Number of Good Leaf Nodes Pairs
+    * 
+    * Time: O(n)
+    * Space: O(n)
+    
+*/
+
+class Solution {
+public:
+
+    int dfs(TreeNode* root, int start,int& ans){
+        if(!root)return 0;
+        int depth = 0;
+
+        int L = dfs(root->left,start,ans);
+        int R = dfs(root->right,start,ans);
+
+        if(root->val == start){
+            depth = -1;
+            ans = max(L,R);
+        }else if(L>=0&&R>=0) depth = max(L,R)+1;
+        else{
+            ans = max(abs(L)+abs(R),ans);
+            depth = min(L,R) -1;
+        }
+        return depth;
+    }
+    int amountOfTime(TreeNode* root, int start) {
+        int ans = 0;
+
+        dfs(root,start,ans);
+
+        return ans;
+    }
+};
