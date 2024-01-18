b29175f67213df16e0cc990ad8dfbfccd828ec88
Tae Lysia Lefebvre <11-KiwiTae@users.noreply.movensee>
2024-01-18 09:15:00 +0100

    leetcode 0118

---
+++ easy/climbing-stairs.cpp
@@ -0,0 +1,19 @@
+ 
+/*
+    leetcode 70. Climbing Stairs
+
+    Time: O(n)
+    Space: O(1)
+*/
+
+class Solution {
+public:
+    int climbStairs(int n) {
+
+        int dp[2] = {1,2};
+        for(int i = 2; i < n; i++){
+            dp[i%2] += dp[(i+1)%2];
+        }
+        return dp[(n-1)%2];
+    }
+};
